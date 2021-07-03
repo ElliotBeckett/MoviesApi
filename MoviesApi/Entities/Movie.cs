c1e991dccec3b6c209816a4412a3c41ffaf493e8
ElliotBeckett <elliotbeckett1@gmail.com>
2021-07-03 18:24:50 +1000

    Removed Azure connection

---
+++ MoviesApi/Entities/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApi.Entities
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+    }
+}
